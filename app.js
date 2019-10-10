dd9e0f508dbb0841dca56adb5e7b875948b5e59e
zaidishahbaz <shahbazzaidi15@gmail.com>
2019-10-10 10:22:03 +0530

    start game function and other buttons

--- app.js
+++ app.js
@@ -4,5 +4,24 @@ new Vue({
         playerHealth: 10,
         monsterHealth: 10,
         gamerunning: false
-    }
+    },
+    methods: {
+        startGame: function () {
+            this.gamerunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        },
+        attack: function () {
+
+        },
+        specialAttack: function () {
+
+        },
+        heal: function () {
+
+        },
+        giveUp: function () {
+
+        }
+    },
 });
